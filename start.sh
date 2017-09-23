5365758e621c78763e925a97947e48ecabbfd44b
Jeremy Evans <openspacehexagon@gmail.com>
2017-09-23 13:47:04 -0500

    adding files from dev machine

---
+++ start.sh
@@ -0,0 +1,34 @@
+
+#/bin/bash
+export x=0
+export f=0
+frames=("")
+while sleep 1;
+do
+	python camera.py;
+	export filename="tape/"$(date +%s)".log";
+	frames[$x]=$filename
+	export jsonFile=$filename".json";
+	jp2a --width=180 image.jpg > $filename;
+	if [ $f -eq 30 ];
+	then
+		echo "saving json video";
+		echo "{ config: { columns: 180 }, frames: [" > $jsonFile;
+		while [ $f -gt -1 ];
+		do
+			echo $($x - $f);
+			echo "\"" >> $jsonFile;
+			cat ${frames[$(expr $x - $f)]} >> $jsonFile;
+			echo "\"," >> $jsonFile;
+			export f=$(($f - 1))
+		done	
+		echo "]}" >> $jsonFile;
+		export f=0;
+	fi
+	cat $filename
+	echo $x
+	echo $f
+	echo "${frames[$x]}"
+	export x=$(($x + 1))
+	export f=$(($f + 1))
+done

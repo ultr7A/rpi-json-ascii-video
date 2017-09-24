a3f61ac62bbeac3c2f4f8264088c6e96fff1e3a9
Jeremy Evans <openspacehexagon@gmail.com>
2017-09-24 11:08:10 -0500

    improving ui

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-
 #/bin/bash
 export x=0
 export f=0
+export clipLength=30
+export frameData=""
+export clip=0
 frames=("")
 while sleep 1;
 do
@@ -10,25 +12,27 @@ do
 	frames[$x]=$filename
 	export jsonFile=$filename".json";
 	jp2a --width=180 image.jpg > $filename;
+	echo "";
 	cat $filename
-	if [ $f -eq 30 ];
+	if [ $f -eq $clipLength ];
 	then
 		echo "saving json video";
-		echo "{ \"config\": { \"columns\": 180 }, \"frames\": [" > $jsonFile;
+		echo -n "{ \"config\": { \"columns\": 180 }, \"frames\": [" > $jsonFile;
 		while [ $f -gt -1 ];
 		do
-			export f=$(($f - 1));
 			echo -n "\"" >> $jsonFile;
-			cat ${frames[$(expr $x - $f)]} >> $jsonFile;
+			cat ${frames[$(expr $x - $f)]}  >> $jsonFile;
 			echo "\"," >> $jsonFile;
+			export f=$(($f - 1))
 		done	
 		echo "]}" >> $jsonFile;
 		export f=0;
+		export clip=$(($clip + 1))
 		cd tape
 		ls | grep -P "\.log$" | xargs -d "\n" rm
 		cd ../
 	fi
-	echo "$x:$f ${frames[$x]}"
+	echo -n "$clip:[$f/$clipLength] ${frames[$x]} "
 	export x=$(($x + 1))
 	export f=$(($f + 1))
 done
